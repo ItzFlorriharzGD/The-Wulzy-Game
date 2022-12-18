2d531fdaccb7465a5f2ef2d2599078526ac4be64
ItzFlorriharzGD <120856606+ItzFlorriharzGD@users.noreply.github.com>
2022-12-17 20:14:39 -0500

    Add files via upload

---
+++ cool html thing/run.js
@@ -0,0 +1,7 @@
+function run() {
+ let ops = "statusbar=off, height=1080, width=1920, fullscreen=on"
+ open("./index.html", "_blank", ops)
+}
+function run2() {
+ open("./download2.html", "_blank")
+}
